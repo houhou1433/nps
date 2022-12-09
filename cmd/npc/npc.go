0ffb59afc1c0db80977082eab3506f8317029880
z1433 <48983153+houhou1433@users.noreply.github.com>
2022-12-09 11:32:32 +0800

    Add files via upload

--- cmd/npc/npc.go
+++ cmd/npc/npc.go
@@ -228,8 +228,7 @@ func run() {
 	if *verifyKey == "" {
 		*verifyKey, _ = env["NPC_SERVER_VKEY"]
 	}
-	//logs.Info("the version of client is %s, the core version of client is %s", version.VERSION, version.GetVersion())
-	//logs.Info("[ok]")
+	logs.Info("the version of client is %s, the core version of client is %s", version.VERSION, version.GetVersion())
 	if *verifyKey != "" && *serverAddr != "" && *configPath == "" {
 		go func() {
 			for {
